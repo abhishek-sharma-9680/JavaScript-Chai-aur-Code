f1b1768e85cab1d05a393c75197087d5d3023774
abhishek-58 <abhishek.21ug1095@iiitranchi.ac.in>
2024-08-18 16:49:29 +0530

    let var differences

--- 01_Basics/01_test.js
+++ 01_Basics/01_test.js
@@ -1,2 +1,20 @@
 console.log("mr.abhi the dashing");
 console.log("mr dashing the rider");
+
+/*In JavaScript, the let keyword allows you to declare variables that are block-scoped, meaning the variable is limited to the block in which it is defined (e.g., inside a loop, function, or conditional). If you try to declare the same variable more than once using let within the same scope, you will get a syntax error because let does not allow re-declaration of the same variable in the same scope.
+
+Example:
+javascript
+Copy code
+let a = 10;
+let a = 5; // SyntaxError: Identifier 'a' has already been declared
+This rule exists to prevent potential bugs that could arise from accidentally redeclaring variables and to encourage cleaner, more predictable code.
+
+var Keyword:
+In contrast, the var keyword is function-scoped or globally-scoped if declared outside a function. When you declare a variable using var, you can redeclare the same variable within the same scope without throwing an error.
+
+Example:
+javascript
+Copy code
+var b = 10;
+var b = 5; // No error, the value of 'b' is simply overwritten*/
